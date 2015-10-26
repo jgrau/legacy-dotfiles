8112fa70c687a68c89482c352017c502c6dfea1d
jgrau <jonas.grau@gmail.com>
2015-10-26 23:48:11 +0100

    Bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Hombrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew tap Homebrew/bundle
+brew bundle
+brew cleanup
+
+rcup
+
+source './osx'
